ab4536b41ddb368b6ef9aeb5662fd96dba8fe3dd
Oopas <69417818+Oopas@users.noreply.github.com>
2021-01-11 12:18:04 +0800

    Update NeoTermActivity.kt

--- app/src/main/java/io/neoterm/ui/term/NeoTermActivity.kt
+++ app/src/main/java/io/neoterm/ui/term/NeoTermActivity.kt
@@ -340,7 +340,8 @@ class NeoTermActivity : AppCompatActivity(), ServiceConnection, SharedPreference
         }
 
         if (!isRecreating()) {
-            if (false) {
+            if (SetupHelper.needSetup()) {
+                return
                 val intent = Intent(this, SetupActivity::class.java)
                 startActivityForResult(intent, REQUEST_SETUP)
                 return
